8a6b3f4e4ee61e343919edf12db3e000db292547
Jacob Colyvan <jacob_colyvan@keemail.me>
2020-03-18 15:15:28 +1100

    Initial working project commited. NOTE: displays price for chosen currency; has old code for using in built http/json methods.

--- blockchain_ticker.rb
+++ blockchain_ticker.rb
@@ -1,32 +1,19 @@
-require 'json'
 require 'httparty'
 require 'tty-prompt'
 # require 'net/http'
+# require 'json'
 
 btcUrl = 'https://api.btcmarkets.net/market/BTC/AUD/tick'
 ethUrl = 'https://api.btcmarkets.net/market/ETH/AUD/tick'
 ltcUrl = 'https://api.btcmarkets.net/market/LTC/AUD/tick'
 
-
 options = ["lastPrice", "volume24h", "bestBid", "bestAsk", "low24h", "high24h"]
-currencies = [BTC, ETH, LTC]
+currencies = ["BTC", "ETH", "LTC"]
 
-
-url = ethUrl
-uri = URI(url)
-
-# url = "https://api.btcmarkets.net/v2/market/active"
-# uri = URI(url)
-
-# response = Net::HTTP.get(uri)
-
-data_hash = JSON.parse(response)
-
-puts data_hash["lastPrice"]
-# puts data_hash
-
+puts `clear`
 prompt = TTY::Prompt.new
-curr = prompt.select("Choose Currency\n", currencies)
+curr = prompt.select("Choose Currency", currencies)
+url = nil
 case curr
 when "BTC"
     url = btcUrl
@@ -35,3 +22,16 @@ when "ETH"
 when "LTC"
     url = ltcUrl
 end
+
+
+# url = ethUrl
+# uri = URI(url)
+# url = "https://api.btcmarkets.net/v2/market/active"
+# response = Net::HTTP.get(uri)
+# data_hash = JSON.parse(response)
+
+data_hash = HTTParty.get(url).parsed_response
+puts data_hash["lastPrice"]
+
+
+
